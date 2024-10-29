65dc405db85709bbca4ab0f2ac9fee4cb923dcb6
Matthias Baitsch <matthias.baitsch@me.com>
2024-10-29 06:35:15 +0100

    Add .lintr

--- w-einzelne-merkmale/folien/skripte/recreate-data-lecture.R
+++ w-einzelne-merkmale/folien/skripte/recreate-data-lecture.R
@@ -8,8 +8,7 @@ d_ns_bochum_tag <- d_ns_bochum_tag |> select(Datum, Niederschlag = NS)
 d_ns_bochum_monat <- d_ns_bochum_monat |> select(Jahr, Monat, Niederschlag = NS)
 
 save(
-  d_wb_all, d_wb_2012, d_wb_countries, d_wb_countries_2012,
-  d_ns_bochum_tag, d_ns_bochum_monat, 
-  file="data/data-lecture.Rdata"
+    d_wb_all, d_wb_2012, d_wb_countries, d_wb_countries_2012,
+    d_ns_bochum_tag, d_ns_bochum_monat,
+    file = "data/data-lecture.Rdata"
 )
-
